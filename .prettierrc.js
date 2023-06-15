6c82852f49e6aa0e5f4b99d303b72f20093ab4ed
Yannic Hammer <it@beatbrackerz.de>
2023-06-15 21:16:12 +0200

    fix(lint): update and execute new prettier config to javascript modules

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: false,
+  bracketSpacing: false,
+  semi: true,
+  singleAttributePerLine: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+};
